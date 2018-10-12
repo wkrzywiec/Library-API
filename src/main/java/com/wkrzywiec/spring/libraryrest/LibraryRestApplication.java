6fc84968b32fdb0de15a7aa8fefcc055f81679ab
Wojtek <wojciechkrzywiec@gmail.com>
2018-10-12 18:29:43 +0200

    Google Cloud Platform config - for App Engine and Cloud SQL

--- src/main/java/com/wkrzywiec/spring/libraryrest/LibraryRestApplication.java
+++ src/main/java/com/wkrzywiec/spring/libraryrest/LibraryRestApplication.java
@@ -2,10 +2,17 @@ package com.wkrzywiec.spring.libraryrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LibraryRestApplication {
+public class LibraryRestApplication extends SpringBootServletInitializer {
 
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		 return application.sources(LibraryRestApplication.class);
+	  }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryRestApplication.class, args);
 	}
